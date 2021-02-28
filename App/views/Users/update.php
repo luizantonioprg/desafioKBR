11c7592da7590bf405f29d10df34f4114f7b8707
luizantonioprg <43731038+luizantonioprg@users.noreply.github.com>
2021-02-28 18:25:21 -0300

    Update update.php

--- App/views/Users/update.php
+++ App/views/Users/update.php
@@ -1,5 +1,5 @@
 <div class="containerLogo">
-	<div class="slogan">
+    <div class="slogan">
       <h1>Corcovado Express</h1>
     </div>
     <div class="imageLogoContainer">
@@ -25,22 +25,22 @@
 
 <form class="login" action="" method="POST" enctype="multipart/form-data">
 
-        <input type="text" placeholder="nome" name="nomeAtualizar">
-        <input type="text" placeholder="email" name="emailAtualizar">
-        <input type="text" placeholder="senha" name="senhaAtualizar">
-        <input type="text" placeholder="privilegio" name="privilegioAtualizar">
+         <input type="text" placeholder="nome" name="nomeAtualizar">
+         <input type="text" placeholder="email" name="emailAtualizar">
+         <input type="text" placeholder="senha" name="senhaAtualizar">
+         <input type="text" placeholder="privilegio" name="privilegioAtualizar">
 
 
 
-        <?php
-       // if(isset($_POST['submitUpdate'])){ 
-            foreach($data['msg'] as $m){
-              echo "<h2 >".$m."</h2>";
+			<?php
+		       // if(isset($_POST['submitUpdate'])){ 
+			    foreach($data['msg'] as $m){
+			      echo "<h2 >".$m."</h2>";
 
-            }
-         // }
+			    }
+			 // }
 
-        ?>
+			?>
 
-				<button type="submit" name="submitUpdate">Atualizar</button>
+	  <button type="submit" name="submitUpdate">Atualizar</button>
         </form>
